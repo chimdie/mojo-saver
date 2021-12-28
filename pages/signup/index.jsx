271745fe1aebcfce37fc14e4a08f0751d7e62bab
chimdi nze <nzechimdi@yahoo.com>
2021-12-28 04:38:14 +0100

    feat: registration and login page

---
+++ pages/signup/index.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Box, FormLabel, FormControl, Input } from "@chakra-ui/react";
+import { RegistrationLayout } from "../../layouts";
+
+export default function Signup() {
+  return (
+    <RegistrationLayout
+      header="Create a Secure Account"
+      subHeader="Explore the secret of saving by contribution"
+      caption="CREATE ACCOUNT"
+    >
+      <FormControl className="mb-5">
+        <FormLabel htmlFor="fullName">Full Name</FormLabel>
+        <Input
+          type="text"
+          placeholder=""
+          paddingY="1.5rem"
+          outline="none"
+          color="#1a202c"
+          bg="#edf2f7"
+          border="0 solid #e2e8f0"
+          borderRadius=".25rem"
+          textShadow="none"
+        />
+      </FormControl>
+      <FormControl className="mb-5">
+        <FormLabel htmlFor="email">Email Address</FormLabel>
+        <Input
+          type="email"
+          placeholder=""
+          paddingY="1.5rem"
+          outline="none"
+          color="#1a202c"
+          bg="#edf2f7"
+          border="0 solid #e2e8f0"
+          borderRadius=".25rem"
+          textShadow="none"
+        />
+      </FormControl>
+      <FormControl className="mb-5">
+        <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
+        <Input
+          type="email"
+          placeholder=""
+          paddingY="1.5rem"
+          outline="none"
+          color="#1a202c"
+          bg="#edf2f7"
+          border="0 solid #e2e8f0"
+          borderRadius=".25rem"
+          textShadow="none"
+        />
+      </FormControl>
+      <FormControl className="mb-5">
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input
+          type="password"
+          placeholder=""
+          paddingY="1.5rem"
+          outline="none"
+          color="#1a202c"
+          bg="#edf2f7"
+          border="0 solid #e2e8f0"
+          borderRadius=".25rem"
+          textShadow="none"
+        />
+      </FormControl>
+      <FormControl className="mb-5">
+        <FormLabel htmlFor="password">Confirm Password</FormLabel>
+        <Input
+          type="password"
+          placeholder=""
+          paddingY="1.5rem"
+          outline="none"
+          color="#1a202c"
+          bg="#edf2f7"
+          border="0 solid #e2e8f0"
+          borderRadius=".25rem"
+          textShadow="none"
+        />
+      </FormControl>
+    </RegistrationLayout>
+  );
+}
