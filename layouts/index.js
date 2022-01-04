1fd84c3879127f39c0a63fe54e75c5523c2f0c77
Chimdie <nzechimdi@yahoo.com>
2022-01-04 01:38:39 +0100

    Revert "Feat/homepage (#4)" (#5)
    This reverts commit 8659e834416d9d75cd28401029ac3c80e748ed78.

--- layouts/index.js
+++ layouts/index.js
@@ -1,5 +1,4 @@
 import RegistrationLayout from "./registrationLayout";
 import DashboardLayout from "./dashboardLayout";
-import MainLayout from "./mainLayout";
 
-export { RegistrationLayout, DashboardLayout, MainLayout };
+export { RegistrationLayout, DashboardLayout };
