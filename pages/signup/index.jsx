af66e9983d19e08c1c3ba64733c10f7ec01f5fb3
chimdi nze <nzechimdi@yahoo.com>
2022-01-22 23:49:07 +0100

    mocking up signup

--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
@@ -26,11 +26,22 @@ export default function SignupPage() {
   );
   const router = useRouter();
 
-  useEffect(() => {
-    if (loadingStatus === "fulfilled" && user.email) {
-      router.push("/login");
+  // useEffect(() => {
+  //   if (loadingStatus === "fulfilled" && user.email) {
+  //     router.push("/login");
+  //   }
+  // }, [loadingStatus, router, user]);
+
+  const handleReg = useCallback(async (e) => {
+    e.preventDefault();
+    const { email, password } = e.target.elements;
+    try {
+      let userDetails = signUp(email.value, password.value);
+      // let data =
+    } catch (error) {
+      alert(error);
     }
-  }, [loadingStatus, router, user]);
+  });
 
   const {
     register,
@@ -41,130 +52,154 @@ export default function SignupPage() {
   });
 
   function registerUser() {
-    dispatch(signUp({ email: "emmar@gmail.com", password: "Africa@40" }));
+    dispatch(signUp({ email: "emar@gmail.com", password: "Africa@40" }));
     console.log("data stored");
   }
+  // const handleSubmit = useCallback(
+  //   async (e) => {
+  //     e.preventDefault();
+  //     const { username, email, password, location } = e.target.elements;
+  //     try {
+  //       let credential = await firebaseSignUp(
+  //         username.value,
+  //         email.value,
+  //         password.value,
+  //         location.value
+  //       );
+  //       let data = await newUser({
+  //         id: credential.user.uid,
+  //         username: username.value,
+  //         email: email.value,
+  //         password: password.value,
+  //         location: location.value,
+  //       });
+  //       console.log({ data });
+  //       history.push("/account/setup");
+  //     } catch (error) {
+  //       alert(error);
+  //     }
+  //   },
+  //   [history]
+  // );
   return (
-    <>
-      <RegistrationLayout
-        header="Create a Secure Account"
-        subHeader="Explore the secret of saving by contribution"
+    <RegistrationLayout
+      header="Create a Secure Account"
+      subHeader="Explore the secret of saving by contribution"
+    >
+      <h4>{loadingStatus}</h4>
+      <Box
+        as="form"
+        display="flex"
+        flexDirection="column"
+        flexGrow="1"
+        flexShrink="1"
+        flexBasis="0"
+        paddingY="1.5rem"
+        minHeight="min-content"
+        onSubmit={handleSubmit(registerUser)}
       >
-        <h4>{loadingStatus}</h4>
-        <Box
-          as="form"
-          display="flex"
-          flexDirection="column"
-          flexGrow="1"
-          flexShrink="1"
-          flexBasis="0"
-          paddingY="1.5rem"
-          minHeight="min-content"
-          onSubmit={handleSubmit(registerUser)}
-        >
-          <FormControl className="mb-5">
-            <FormLabel htmlFor="fullName">Full Name</FormLabel>
-            <Input
-              {...register("fullName")}
-              id="fullName"
-              type="text"
-              paddingY="1.5rem"
-              outline="none"
-              color="#1a202c"
-              bg="#edf2f7"
-              border="0 solid #e2e8f0"
-              borderRadius=".25rem"
-              textShadow="none"
-            />
-            {errors.fullName && (
-              <p className="text-sm italic text-red-500">
-                {errors.fullName.message}
-              </p>
-            )}
-          </FormControl>
-          <FormControl className="mb-5">
-            <FormLabel htmlFor="email">Email Address</FormLabel>
-            <Input
-              {...register("email")}
-              id="email"
-              type="email"
-              paddingY="1.5rem"
-              outline="none"
-              color="#1a202c"
-              bg="#edf2f7"
-              border="0 solid #e2e8f0"
-              borderRadius=".25rem"
-              textShadow="none"
-            />
-            {errors.email && (
-              <p className="text-sm italic text-red-500">
-                {errors.email.message}
-              </p>
-            )}
-          </FormControl>
-          <FormControl className="mb-5">
-            <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
-            <Input
-              {...register("phoneNumber")}
-              id="phoneNumber"
-              type="tel"
-              paddingY="1.5rem"
-              outline="none"
-              color="#1a202c"
-              bg="#edf2f7"
-              border="0 solid #e2e8f0"
-              borderRadius=".25rem"
-              textShadow="none"
-            />
-            {errors.phoneNumber && (
-              <p className="text-sm italic text-red-500">
-                {errors.phoneNumber.message}
-              </p>
-            )}
-          </FormControl>
-          <FormControl className="mb-5">
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              {...register("password")}
-              id="password"
-              type="password"
-              paddingY="1.5rem"
-              outline="none"
-              color="#1a202c"
-              bg="#edf2f7"
-              border="0 solid #e2e8f0"
-              borderRadius=".25rem"
-              textShadow="none"
-            />
-            {errors.password && (
-              <p className="text-sm italic text-red-500">
-                {errors.password.message}
-              </p>
-            )}
-          </FormControl>
-          <FormControl className="mb-5">
-            <FormLabel htmlFor="password">Confirm Password</FormLabel>
-            <Input
-              {...register("confirmPassword")}
-              id="confirmPassword"
-              type="password"
-              paddingY="1.5rem"
-              outline="none"
-              color="#1a202c"
-              bg="#edf2f7"
-              border="0 solid #e2e8f0"
-              borderRadius=".25rem"
-              textShadow="none"
-            />
-            {errors.confirmPassword && (
-              <p className="text-sm italic text-red-500">
-                {errors.confirmPassword.message}
-              </p>
-            )}
-          </FormControl>
-          <AuthBtn caption="CREATE ACCOUNT" />
-        </Box>
-      </RegistrationLayout>
-    </>
+        <FormControl className="mb-5">
+          <FormLabel htmlFor="fullName">Full Name</FormLabel>
+          <Input
+            {...register("fullName")}
+            id="fullName"
+            type="text"
+            paddingY="1.5rem"
+            outline="none"
+            color="#1a202c"
+            bg="#edf2f7"
+            border="0 solid #e2e8f0"
+            borderRadius=".25rem"
+            textShadow="none"
+          />
+          {errors.fullName && (
+            <p className="text-sm italic text-red-500">
+              {errors.fullName.message}
+            </p>
+          )}
+        </FormControl>
+        <FormControl className="mb-5">
+          <FormLabel htmlFor="email">Email Address</FormLabel>
+          <Input
+            {...register("email")}
+            id="email"
+            type="email"
+            paddingY="1.5rem"
+            outline="none"
+            color="#1a202c"
+            bg="#edf2f7"
+            border="0 solid #e2e8f0"
+            borderRadius=".25rem"
+            textShadow="none"
+          />
+          {errors.email && (
+            <p className="text-sm italic text-red-500">
+              {errors.email.message}
+            </p>
+          )}
+        </FormControl>
+        <FormControl className="mb-5">
+          <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
+          <Input
+            {...register("phoneNumber")}
+            id="phoneNumber"
+            type="tel"
+            paddingY="1.5rem"
+            outline="none"
+            color="#1a202c"
+            bg="#edf2f7"
+            border="0 solid #e2e8f0"
+            borderRadius=".25rem"
+            textShadow="none"
+          />
+          {errors.phoneNumber && (
+            <p className="text-sm italic text-red-500">
+              {errors.phoneNumber.message}
+            </p>
+          )}
+        </FormControl>
+        <FormControl className="mb-5">
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            {...register("password")}
+            id="password"
+            type="password"
+            paddingY="1.5rem"
+            outline="none"
+            color="#1a202c"
+            bg="#edf2f7"
+            border="0 solid #e2e8f0"
+            borderRadius=".25rem"
+            textShadow="none"
+          />
+          {errors.password && (
+            <p className="text-sm italic text-red-500">
+              {errors.password.message}
+            </p>
+          )}
+        </FormControl>
+        <FormControl className="mb-5">
+          <FormLabel htmlFor="password">Confirm Password</FormLabel>
+          <Input
+            {...register("confirmPassword")}
+            id="confirmPassword"
+            type="password"
+            paddingY="1.5rem"
+            outline="none"
+            color="#1a202c"
+            bg="#edf2f7"
+            border="0 solid #e2e8f0"
+            borderRadius=".25rem"
+            textShadow="none"
+          />
+          {errors.confirmPassword && (
+            <p className="text-sm italic text-red-500">
+              {errors.confirmPassword.message}
+            </p>
+          )}
+        </FormControl>
+        <AuthBtn caption="CREATE ACCOUNT" />
+      </Box>
+    </RegistrationLayout>
   );
 }
