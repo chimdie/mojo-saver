bd088027f8bad759875a39ebf4eb6fc7e6410331
chimdi nze <nzechimdi@yahoo.com>
2022-01-22 23:49:45 +0100

    adds login slice

--- redux/account/index.js
+++ redux/account/index.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { fireBaseSignUp } from "../../firebase/auth";
+import { fireBaseSignUp, firebaseLogin } from "../../firebase/auth";
 import { HTTP_STATUS } from "../../utils";
 
 const initialState = {
@@ -28,21 +28,11 @@ export const signUp = createAsyncThunk(
   }
 );
 
-// export const signUp = createAsyncThunk(
-//   `${NAME_SPACE}/signup`,
-//   async (_data) => {
-//     const { data } = await Api.post("/auth/signup", _data);
-//     return data;
-//   }
-// );
-
-export const login = createAsyncThunk(
-  "users/fetchByIdStatus",
-  async (_data) => {
-    const { data } = await Api.post("/auth/login", _data);
-    return data;
-  }
-);
+export const login = createAsyncThunk("users/fetchByIdStatus", async (data) => {
+  // const { data } = await Api.post("/auth/login", _data);
+  // return data;
+  firebaseLogin();
+});
 
 export const AccountSlice = createSlice({
   name: "user",
@@ -53,47 +43,48 @@ export const AccountSlice = createSlice({
     // },
   },
   extraReducers: (builder) => {
+    // SIGNUP
     //when the request is sent and still pending
-    builder.addCase(signUp.pending, (state, action) => {
-      // Add user to the state array
-      console.log("pending");
-      state.loadingStatus = "pending";
-
-      state.user = action.payload;
-    });
-
-    // Add reducers for additional action types here, and handle loading state as needed
-    builder.addCase(signUp.fulfilled, (state, action) => {
-      // Add user to the state array
-      console.log("fulfilled");
-      state.loadingStatus = "fulfilled";
-      state.user = action.payload;
-    });
-
-    // builder
-    //   .addCase(signUp.pending, (state, action) => {
-    //     state.loadingStatus = HTTP_STATUS.LOADING;
-    //   })
-    //   .addCase(signUp.fulfilled, (state, action) => {
-    //     state.loadingStatus = HTTP_STATUS.DONE;
-    //     if (action.payload.success) {
-    //       state.user = action.payload.result;
-    //     } else {
-    //       toast.error(action.payload.message);
-    //     }
-    //   })
-    //   .addCase(signUp.rejected, (state, { error }) => {
-    //     state.loadingStatus = HTTP_STATUS.ERROR;
-    //     toast.error("server error");
-    //   });
-
-    //when there is an error with the request
-    builder.addCase(signUp.rejected, (state, action) => {
-      // Add user to the state array
-      console.log("rejected");
-      state.loadingStatus = "rejected";
-      state.user = action.payload;
-    });
+    builder
+      .addCase(signUp.pending, (state, action) => {
+        // Add user to the state array
+        console.log("pending");
+        state.loadingStatus = "pending";
+        state.user = action.payload;
+      })
+      // Add reducers for additional action types here, and handle loading state as needed
+      .addCase(signUp.fulfilled, (state, action) => {
+        // Add user to the state array
+        console.log("fulfilled");
+        state.loadingStatus = "fulfilled";
+        state.user = action.payload;
+      })
+      //when there is an error with the request
+      .addCase(signUp.rejected, (state, action) => {
+        // Add user to the state array
+        console.log("rejected");
+        state.loadingStatus = "rejected";
+        state.user = action.payload;
+      });
+  },
+  // LOGIN
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state, action) => {
+        state.loadingStatus = "pending";
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loadingStatus = "fulfilled";
+        // if (action.payload.success) {
+        //   state.user = { ...action.payload.result };
+        // } else {
+        //   toast.error(action.payload.message);
+        // }
+      })
+      .addCase(login.rejected, (state, { error }) => {
+        state.loadingStatus = "rejected";
+        // toast.error("server error");
+      });
   },
 });
 
