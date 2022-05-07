df96de8189315c469c0fb003ded435375f454d44
chimdi nze <nzechimdi@yahoo.com>
2022-05-07 23:52:03 +0100

    feat: user viewing available groups

--- pages/dashboard/groups/index.jsx
+++ pages/dashboard/groups/index.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Box } from "@chakra-ui/react";
 import { DashboardLayout } from "../../../layouts";
 import { GroupCard } from "../../../components/dashboard";
+import { getGroupList } from "../../../redux/group";
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
+
+  const { groups } = useSelector((state) => state.group);
+
+  useEffect(() => {
+    dispatch(getGroupList());
+  }, []);
   return (
     <DashboardLayout>
-      <Box className="shadow-md rounded-md bg-slate-50 overflow-hidden max-w-md p-3">
-        <Box width="35%"></Box>
+      <Box className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {groups.map((group) => {
+          return (
+            <div key={group.id} className="py-2">
+              <GroupCard
+                title={group.name}
+                amount={group.monthlyDepositAmount}
+                description={group.description}
+                id={group.id}
+              />
+            </div>
+          );
+        })}
       </Box>
-      <GroupCard
-        title="frank's million"
-        description="for our first 1 million naira"
-      />
     </DashboardLayout>
   );
 }
