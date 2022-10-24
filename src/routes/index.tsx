ef9cead6df1378f0ab78b246dd59202901321e96
chimdi nze <nzeyoungwealth@gmail.com>
2022-10-24 12:52:20 +0100

    feat: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,4 @@
-import { getLogedInUser } from "pages/auth/slices/authSlice";
-import * as React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "redux/hook";
-import { userData } from "utils";
+import ProtectedRoute from "./protectedRoutes";
+import AppRoutes from "./routes";
 
-export const ProtectedRoute = ({
-  children
-}: {
-  children?: React.ReactNode;
-}) => {
-  const dispatch = useAppDispatch();
-  const { user } = useAppSelector((state) => state.account);
-  const userId = userData() && userData().user ? userData().user : user;
-
-  React.useEffect(() => {
-    if (!userId && userId) {
-      dispatch(getLogedInUser());
-    }
-  }, []);
-
-  if (!userId) {
-    return <Navigate to="/login" replace={true} />;
-  }
-  return <React.Fragment>{children ? children : <Outlet />}</React.Fragment>;
-};
+export { ProtectedRoute, AppRoutes };
