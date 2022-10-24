242cf5f1ee3cf55356af2986626def918c0f01e5
chimdi nze <nzeyoungwealth@gmail.com>
2022-10-24 23:59:45 +0100

    fix: user join group

--- src/components/cards/groupCard.tsx
+++ src/components/cards/groupCard.tsx
@@ -1,13 +1,11 @@
 import React, { MouseEventHandler } from "react";
-import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Image, Text } from "@chakra-ui/react";
 import { landscapeImg, leavesImg, dancersImg, dancerImg } from "assets/card";
 
 type CardProps = {
   title: string;
   description: string;
-  // img?: any;
   onClick?: MouseEventHandler<HTMLDivElement> & MouseEventHandler<HTMLElement>;
-  // joinButton?: React.ReactElement;
 };
 
 const GroupCard = ({ title, description, onClick }: CardProps): JSX.Element => {
@@ -16,16 +14,23 @@ const GroupCard = ({ title, description, onClick }: CardProps): JSX.Element => {
   return (
     <Box
       as="section"
-      className="flex flex-col overflow-hidden w-full max-w-sm bg-white rounded-xl shadow-lg"
+      className="flex flex-col overflow-hidden w-full max-w-xs bg-white rounded-xl shadow-lg hover:bg-slate-50 hover:ease-in-out hover:cursor-pointer"
       onClick={onClick}
     >
       <Image src={randomImg} alt={title} />
       <Box as="section" className="p-4 flex flex-col justify-between h-full">
-        <Heading size={{ base: "md" }} className="py-2">
+        <Heading size={{ base: "sm" }} className="py-1">
           {title}
         </Heading>
-        <Text className="py-2">{description}</Text>
+        <Text className="py-1 text-xs">{description}</Text>
       </Box>
+      <Button
+        borderRadius="none"
+        bg="whitesmoke"
+        className="py-4 rounded-none flex justify-end bg-none"
+      >
+        View members
+      </Button>
     </Box>
   );
 };
