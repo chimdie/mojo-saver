250ceb00bbd1c36741fbfa9f89054a44bbaacd60
chimdi nze <nzeyoungwealth@gmail.com>
2023-04-13 23:51:28 +0100

    fix: paystack key

--- src/components/paystack.tsx
+++ src/components/paystack.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import { Button } from "@chakra-ui/react";
 import { usePaystackPayment } from "react-paystack";
 
+const publicKey = import.meta.env.VITE_PAYSTACK_PUBLIC_KEY;
+
+interface PaystackI {
+  emailAddress: string;
+  amount: number;
+  callBackFn?: () => void;
+}
+
 export default function PayStackApp({
   emailAddress,
   amount,
   callBackFn
-}: {
-  emailAddress: string;
-  amount: string;
-  callBackFn?: () => void;
-}) {
+}: PaystackI) {
   const config = {
     reference: new Date().getTime().toString(),
     email: emailAddress,
     amount: +amount,
     text: "Pay with Paystack",
-    publicKey: "pk_test_b4e015b240dc7ac84d2c67791209a6e60e1fc0c8",
+    publicKey: publicKey,
     label: "string"
   };
 
@@ -35,16 +39,14 @@ export default function PayStackApp({
   const initializePayment = usePaystackPayment(config);
 
   return (
-    <div className="App">
-      <Button
-        onClick={() => {
-          initializePayment(onSuccess, onClose);
-        }}
-        bg="inherit"
-        _hover={{ bg: "inherit" }}
-      >
-        Make Payment
-      </Button>
-    </div>
+    <Button
+      onClick={() => {
+        initializePayment(onSuccess, onClose);
+      }}
+      bg="inherit"
+      _hover={{ bg: "inherit" }}
+    >
+      Make Payment
+    </Button>
   );
 }
